6cb125e68bc80abea340792788fbdd235d77e7cf
Afifa9 <114986359+Afifa9@users.noreply.github.com>
2022-10-09 11:26:01 +0700

    Add files via upload

---
+++ Perkalian.java
@@ -0,0 +1,11 @@
+package simpleant;
+
+public class Perkalian {
+   public static void main(String[] args) {
+	int a,b,hasil;
+	a=20;
+	b=150;
+	hasil=a*b;
+      System.out.println(hasil);
+   }
+}
